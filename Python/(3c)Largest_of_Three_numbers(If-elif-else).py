bfb4eeef8a469c4a7f17da0f85d5e23cf2949b1c
Dhananjaya Roul <dhananjaya032007@gmail.com>
2026-04-21 07:40:23 +0530

    Added files:     Python/(3c)Largest_of_Three_numbers(If-elif-else).py     Python/(3d)Print_1to5_using_while_loop.py     Python/(3e)Print_1to5_Using_for_loop.py     Python/(3f)Sum_of_first_N_Numbers.py     Python/(3g)Multiplication_table.py     Python/(3h)Count_digits_in_number.py     Python/(3i)Factorial_using_loop.py

---
+++ Python/(3c)Largest_of_Three_numbers(If-elif-else).py
@@ -0,0 +1,9 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+c=int(input("Enter the third number: "))
+if a>b and a>c:
+    print ("Largest:",a)
+elif b>c:
+    print ("Largest:",b)
+else:
+    print ("Largest:",c)
