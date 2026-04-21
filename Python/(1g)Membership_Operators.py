03c52f338de9fa5b1dbc570b9269c169f4341f50
Dhananjaya Roul <dhananjaya032007@gmail.com>
2026-04-21 07:46:35 +0530

    added: Python/(1g)Membership_Operators.py

--- Python/(1g)Membership_Operators.py
+++ Python/(1g)Membership_Operators.py
@@ -1,3 +1,6 @@
+#Program to check membership in a list
 numbers=[10,20,30,40,50]
+#check if 20 is in the list
 print("Is 20 in list?",20 in numbers)
+#check if 60 is in the list
 print("Is 60 in list?",60 in numbers)
