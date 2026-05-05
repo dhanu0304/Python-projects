325344796076d613622e973cd6a5cb032df36dae
Dhananjaya Roul <dhananjaya032007@gmail.com>
2026-05-05 07:47:54 +0530

    added files to python folder

---
+++ Python/listmaxdemo.py
@@ -0,0 +1,6 @@
+#max() : In python, the max() function is used to find the maximum value from a list or any iterable. It can also be used to find the maximum of two or more arguments.
+#For string items, the string with higher ASCII value is considered as maximum. For example, 'b' is greater than 'a' because the ASCII value of 'b' is 98 and the ASCII value of 'a' is 97.
+list1=[10, 20, 30, 40, 50]
+list2=['java',  'c','python', 'cpp','sql', 'SQL']
+print("Max of list1:", max(list1))
+print("Max of list2:", max(list2))
---
+++ Python/listmindemo.py
@@ -0,0 +1,6 @@
+#min(): In python the min() function is used to find the minimum value in the list.
+#For string items, the string with lower ASCII value is considered as minimum. For example, 'a' is less than 'b' because the ASCII value of 'a' is 97 and the ASCII value of 'b' is 98.
+num= [10, 20, 30, 40, 50]
+lang = ['java',  'c','python', 'cpp','sql', 'SQL']
+print("Min of num:", min(num))
+print("Min of lang:", min(lang))
